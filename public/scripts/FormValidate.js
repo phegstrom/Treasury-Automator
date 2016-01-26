204bc869766bd4d5e2bbbc8117d43ef6a655185e
Parker Hegstrom <parker.hegstrom@gmail.com>
2016-01-26 01:22:27 -0500

    sign up form arts and crafts

--- public/scripts/FormValidate.js
+++ public/scripts/FormValidate.js
@@ -1,9 +1,13 @@
 // // JS For form validation on sign up form
 
+
 var parsley_options = {
 	successClass: 'valid',
 	errorClass: 'invalid'
 };
+
+
+// for sign up form - options
 
 var nameOptions = [
 				{selector: '#for_name', attribute: 'data-parsley-required', value: ''},
@@ -25,17 +29,34 @@ var reenterPasswordOptions = [
 				{selector: '#for_password-1', attribute: 'data-parsley-required', value: ''},
 				{selector: '#for_password_1_label', attribute: 'data-error', value: 'Passwords don\'t match'},				
 				{selector: '#for_password-1', attribute: 'data-parsley-equalto', value: '#for_password'}
-				];									
+				];
 
+// For login form options												
 
+var loginEmailOptions = [
+				{selector: '#for_email_login', attribute: 'data-parsley-required', value: ''},
+				{selector: '#for_email', attribute: 'data-parsley-type', value: 'email'},
+				{selector: '#for_email', attribute: 'type', value: 'email'},				
+				];
+
+var loginPasswordOptions = [
+				{selector: '#for_password_login', attribute: 'data-parsley-required', value: ''}
+				];				
+
+// bind forms to parsley
 var myParsleyForm = $('#signup-form').parsley(parsley_options);
 $('#signup-form').attr('data-parsley-errors-messages-disabled', '');
 
+var loginParsleyForm = $('#login-form').parsley(parsley_options);
+$('#login-form').attr('data-parsley-errors-messages-disabled', '');
 
+// set options for form fields
 setParsleyAttributes(nameOptions);
 setParsleyAttributes(emailOptions);
 setParsleyAttributes(passwordOptions);
 setParsleyAttributes(reenterPasswordOptions);
+setParsleyAttributes(loginEmailOptions);
+setParsleyAttributes(loginPasswordOptions);
 
 
 // $('#signup-form').on('change', function() {
